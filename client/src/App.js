e13f0ed0a1d8eb8c03730b4289dde39e21ed28bd
Osmangazii <osmangazi.metin@hotmail.com>
2025-04-20 20:10:23 +0300

    client eklendi

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TarkovQuestTracker from "./components/TarkovQuestTracker"; // doğru path olduğundan emin ol
+
+function App() {
+  return (
+    <div>
+      <TarkovQuestTracker />
+    </div>
+  );
+}
+
+export default App;
